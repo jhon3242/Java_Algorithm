beb3799a89ebe99b1b3d1adbc082f1c461785a28
jhon3242 <rldotnr2@gmail.com>
2022-09-02 18:14:53 +0900

    [add] | backjoon 빗물 14719 https://www.acmicpc.net/problem/14719 배열을 탐색하면서 후보들을 구하고 해당 후보들 중에서 필요 없는 후보를 제외하여 최종 후보를 구하고 해당 기둥들 사이에 있는 부분의 빗물의 개수를 구했다. 더 좋은 풀이로는 그냥 왼쪽부터 큰 값, 오른쪽 부터 큰 값을 구한다음 둘중 작은 값을 빼는 방식이 있다.

--- src/Pro_92334.java
+++ src/Pro_92334.java
@@ -1,16 +1,22 @@
 import programmers.level2.p118667.Solution2;
 
-
+import java.util.*;
 public class Pro_92334 {
 
 
 	public static void main(String[] args) {
 
 		int[] q1 = {3, 2, 7, 2};
-		int[] q2 = {4, 6, 5, 1};
-		Solution2 solution = new Solution2();
-		solution.solution(q1, q2);
+		ArrayList<Integer> arrList = new ArrayList<>();
 
+		arrList.add(3);
+		arrList.add(2);
+		arrList.add(7);
+		arrList.add(2);
+
+		int i = arrList.indexOf(7);
+		System.out.println(i);
+		int asInt = Arrays.stream(q1).max().getAsInt(); // 7
 	}
 
 
---
+++ src/baekjoon/p14719/Main.java
@@ -0,0 +1,121 @@
+package baekjoon.p14719;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		ArrayList<Integer> candidate = new ArrayList<>();
+
+		st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		String[] splited = br.readLine().split(" ");
+		int[] arr = Arrays.stream(splited).mapToInt(Integer::parseInt).toArray();
+
+		// add candidate
+		addCandidate(candidate, arr);
+
+		// filter candidate
+		filterCandidate(candidate, arr);
+
+//		candidate.forEach(i -> System.out.print(i + " "));
+
+		if (candidate.size() < 2)
+			System.out.println(0);
+		else{
+			int i = 0;
+			int result = 0;
+			while (i < candidate.size() - 1){ // 기둥이 2개이면 1번 3개이면 2번 해야하기 때문
+				int leftIdx = candidate.get(i);
+				int rightIdx = candidate.get(i + 1);
+				int target = Math.min(arr[leftIdx], arr[rightIdx]);
+//				System.out.println("target = " + target);
+//				System.out.println("q = " + q);
+//				System.out.println("p = " + p);
+				for (int j = leftIdx + 1; j < rightIdx; j++) {
+					if (arr[j] < target){
+						result += target - arr[j];
+					}
+					else
+						break;
+				}
+				i++;
+			}
+			System.out.println(result);
+		}
+	}
+
+	private static void filterCandidate(ArrayList<Integer> candidate, int[] arr) {
+		int canIdx = 0;
+		while (true){
+			int leftBlock = arr[candidate.get(canIdx)];
+			int rightMaxSize = -1;
+//			System.out.println("canIdx = " + canIdx);
+			for (int i = canIdx + 1; i < candidate.size(); i++) {
+				rightMaxSize = Math.max(rightMaxSize, arr[candidate.get(i)]);
+			}
+//			System.out.println("leftBlock = " + leftBlock);
+//			System.out.println("rightMaxSize = " + rightMaxSize);
+			boolean flag = true;
+			for (int i = canIdx + 1; i < candidate.size(); i++) {
+				int blockSize = arr[candidate.get(i)];
+//				System.out.println("blockSize = " + blockSize);
+				if (blockSize >= leftBlock || blockSize == rightMaxSize){
+					canIdx = i;
+					flag = false;
+					break;
+				}
+				else{
+					candidate.set(i, -1);
+				}
+			}
+//			candidate.forEach(i -> System.out.print(i + " "));
+			if (flag)
+				break;
+		}
+		candidate.removeIf(i -> i == -1);
+	}
+
+	private static void addCandidate(ArrayList<Integer> candidate, int[] arr) {
+		int pastIdx = 0;
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[pastIdx] > arr[i])
+				candidate.add(pastIdx);
+			else if (i == arr.length - 1)
+				candidate.add(i);
+			pastIdx = i;
+		}
+	}
+}
+
+/*
+TestCase
+
+4 4
+2 2 2 2
+result : 0
+
+4 4
+1 2 3 4
+result : 0
+
+4 4
+4 3 1 2
+result : 1
+
+4 8
+4 1 2 3 2 2 4
+result : 10
+
+4 9
+10 1 3 1 5 1 2 1 3
+후보 0, 2, 4, 6, 8
+result : 15
+
+*
+* */
