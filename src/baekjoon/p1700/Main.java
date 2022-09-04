466d059c7c9f26b27bb14325bc2d7ed93a0df21c
jhon3242 <rldotnr2@gmail.com>
2022-09-04 16:22:04 +0900

    [add] | backjoon 멀티탭 스케줄링 1700 https://www.acmicpc.net/problem/1700 그리디로 구현했으며 전기용품들 배열을 하나씩 받으면서 멀티탭 배열에 넣어주고 멀티탭에 꽂혀 있지 않은 전기 용품이면서 동시에 멀티탭이 꽉찬 경우 그 뒤로 가장 최근에 오지 않는 애를 빼주었다. 어차피 하나를 빼야 하는데 그 뒤에 여러번 오든 안오든 상관이 없다.

---
+++ src/baekjoon/p1700/Main.java
@@ -0,0 +1,82 @@
+package baekjoon.p1700;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		LinkedList<Integer> multiTap = new LinkedList<>();
+
+		final int N = Integer.parseInt(st.nextToken()); // tap size
+		final int K = Integer.parseInt(st.nextToken()); // use count
+		String[] splitString = br.readLine().split(" ");
+		ArrayList<Integer> numArr = new ArrayList<>();
+		int result = 0;
+
+		// init
+		for(String num : splitString){
+			int i = Integer.parseInt(num);
+			numArr.add(i);
+		}
+
+		for (int i = 0; i < numArr.size(); i++) {
+			Integer num = numArr.get(i);
+			if (multiTap.contains(num)) {
+//				System.out.println("include num = " + num);
+				continue;
+			}
+//			System.out.println("multiTap.size() = " + multiTap.size());
+			if (multiTap.size() >= N){ // 빼야하는 작업이 필요함
+
+				LinkedList<Integer> candidate = (LinkedList<Integer>)  multiTap.clone();
+				for (int j = i + 1; j < numArr.size(); j++) {
+					Integer nextNum = numArr.get(j);
+					if (multiTap.contains(nextNum)){
+						candidate.remove(nextNum);
+////						System.out.println("remove candidate nextNum = " + nextNum);
+						if (candidate.size() <= 1)
+							break;
+					}
+				}
+//				System.out.println("candidate.size() = " + candidate.size());
+				Integer target;
+				if (candidate.size() > 0){
+					target = candidate.pop();
+				}
+				else{
+					target = multiTap.pop();
+				}
+//				System.out.println("remove target = " + target);
+				multiTap.remove(target);
+				result++;
+			}
+//			System.out.println("add num = " + num);
+			multiTap.add(num);
+		}
+
+		System.out.println(result);
+	}
+}
+
+/**
+
+3 10
+1 2 3 4 4 5 2 1 1 4
+result : 3
+
+3 5
+1 1 1 1 1
+result : 0
+
+1 10
+1 2 3 4 5 6 7 1 2 3
+result : 9
+
+*/
