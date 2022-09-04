33828305e80e434e7142e9e8d0e5b35ea5eba3b6
jhon3242 <rldotnr2@gmail.com>
2022-09-05 02:39:08 +0900

    [add] | backjoon 가르침 1062 https://www.acmicpc.net/problem/1062 결국 다른사람 코드를 참고하여 BFS 로 문제를 해결했다. combination 을 해야하겠다는 생각에 복잡한 연산을 했었는데 그러한 작업들을 다 없애고 bfs 로만 구현하는 편이 더 깔끔하고 시간초과도 안나는듯하다.

--- src/baekjoon/p1062/Main.java
+++ src/baekjoon/p1062/Main.java
@@ -6,100 +6,70 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-	public static List<Character> learn = new ArrayList<>(30);
-	public static List<Character> candidate = new ArrayList<>(25);
-	public static List<String> words = new ArrayList<>(55);
+	public static boolean [] visited = new boolean[26];
+	public static ArrayList<String> words = new ArrayList<>(50);
+	public static int N;
+	public static int K;
+	public static int maxCount = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		final int N = Integer.parseInt(st.nextToken());
-		final int K = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
 		// a n t i c
-		learn.add('a');
-		learn.add('n');
-		learn.add('t');
-		learn.add('i');
-		learn.add('c');
+		visited['a' - 'a'] = true;
+		visited['n' - 'a'] = true;
+		visited['t' - 'a'] = true;
+		visited['i' - 'a'] = true;
+		visited['c' - 'a'] = true;
 
 		if (K < 5){
 			System.out.println(0);
 			return;
 		}
-
-		// add to array
-		for (int i = 0; i < N; i++) {
-			String word = br.readLine();
-			if (word.length() <= 8)
-				continue;
-			word = word.substring(4, word.length()- 4);
-//			System.out.println("word = " + word);
-
-			// add candidate character
-			for (int j = 0; j < word.length(); j++) {
-				char wordCharacter = word.charAt(j);
-				if (candidate.contains(wordCharacter))
-					continue;
-				candidate.add(wordCharacter);
-//				System.out.println("wordCharacter = " + wordCharacter);
-			}
-
-			// add substring word
-			words.add(word);
+		else if (K >= 26){
+			System.out.println(N);
+			return;
 		}
-
-		Combination com = new Combination(candidate.size(), K - 5);
-		com.combinationWithOp(0, 0);
-		System.out.println(com.maxCount);
-	}
-	static class Combination{
-		int n;
-		int r;
-		int[] result;
-		int maxCount = 0;
-
-		public Combination(int n, int r) {
-			this.n = n;
-			this.r = r;
-			result = new int[r];
-		}
-
-		public void combinationWithOp(int cnt, int idx){
-			if (cnt == r){
-				// picked random idx
-				for (int i : result){
-					Character newChar = candidate.get(i);
-					learn.add(newChar);
-				}
-				int matchCount = 0;
-				for(String word : words){
-					if (matchCk(word))
-						matchCount++;
-				}
-				maxCount = Math.max(maxCount, matchCount);
-				for (int i : result){
-					Character newChar = candidate.get(i);
-					learn.remove(newChar);
-				}
-				return;
+		else{
+			for (int i = 0; i < N; i++) {
+				String word = br.readLine();
+				word = word.substring(4, word.length() - 4);
+				words.add(word);
 			}
-
-			for (int i = idx; i < n; i++) {
-				result[cnt] = i;
-				combinationWithOp(cnt + 1, idx + 1);
-			}
+			bfs(0, 0);
+			System.out.println(maxCount);
 		}
 	}
-
-	private static boolean matchCk(String word) {
-		for (int i = 0; i < word.length(); i++) {
-			char c = word.charAt(i);
-			if (!learn.contains(c))
-				return false;
+	public static void bfs(int start, int count){
+		if (count == K - 5){
+			int matchCount = 0;
+			for (int i = 0; i < words.size(); i++) {
+				String wd = words.get(i);
+				boolean flag = true;
+				for (int j = 0; j < wd.length(); j++) {
+					if (!visited[wd.charAt(j) - 'a']) {
+						flag = false;
+						break;
+					}
+				}
+				if (flag){
+					matchCount++;
+				}
+			}
+			maxCount = Math.max(maxCount, matchCount);
 		}
-		return true;
+
+		for (int i = start; i < 26; i++) {
+			if (!visited[i]){
+				visited[i] = true;
+				bfs(i, count + 1);
+				visited[i] = false;
+			}
+		}
 	}
 }
 
