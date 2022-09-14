2a67bf4b3bb3785c6bc515d0421fa72da79dcabe
jhon3242 <rldotnr2@gmail.com>
2022-09-14 15:49:03 +0900

    [add] | backjoon 동전1 2293 실패 https://www.acmicpc.net/problem/2293 감도 안온다.

---
+++ src/baekjoon/p2293/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.p2293;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, k;
+	static boolean[] canMake;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		canMake = new boolean[k + 1];
+
+		for (int i = 0; i < n; i++) {
+			int coin = Integer.parseInt(br.readLine());
+			int j = 0;
+			while (j * coin <= k){
+				canMake
+			}
+		}
+	}
+}
