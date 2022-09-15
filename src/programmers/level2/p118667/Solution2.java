ba264d48527cc6b39ec381c3dd52acca84628dc0
jhon3242 <rldotnr2@gmail.com>
2022-09-15 18:08:09 +0900

    [fix] | programmers 두 큐 합 같게 만들기 118667 실패 https://school.programmers.co.kr/learn/courses/30/lessons/118667 다시 시도 했는데 또 실패했다.

--- src/programmers/level2/p118667/Solution2.java
+++ src/programmers/level2/p118667/Solution2.java
@@ -7,85 +7,48 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class Solution2 {
-	/** 원형 리스트를 사용해서 해보려고 했는데 생각해보니까 최소 횟수를 찾기는 어려울듯??
-	 *  그러면 모든 경우의 수를 배열에 넣고 각각의 값을 구한 뒤에 최소 횟수를 찾으면 되는거 아닌가?
-	 */
-	private int[] queue1;
-	private int[] queue2;
-	public int solution(int[] queue1, int[] queue2) {
-		int target = 0;
-		int size = 0;
-		this.queue1 = queue1;
-		this.queue2 = queue2;
+	import java.util.*;
 
-		ArrayList<Integer> arr = new ArrayList<>();
+	class Solution {
+		private int[] q1;
+		private int[] q2;
+		private int target, start, end, tmp, q1Size, q2Size;
 
-		for(int i = 0; i < queue1.length; i++){
-			arr.add(queue1[i]);
-			target += queue1[i];
-		}
-
-		for(int i = 0; i < queue2.length; i++){
-			arr.add(queue2[i]);
-			target += queue2[i];
-		}
-
-
-		target /= 2;
-		size = arr.size();
-		return getCandidate(target, size, arr);
-	}
-
-	private int getMoveCount(int i, int count){
-		int len = queue1.length;
-		int result = 0;
-		if (i >= len){
-			i -= len;
-		}
-		if (i + count < len){
-			result += i + count;
-			result += i + len;
-		}
-		else{
-			if (i > 0){
-				result += i;
-			}
-			result += i + count - len;
-		}
-
-		return result;
-	}
-
-	private int getCandidate(int target, int size, ArrayList<Integer> arr) {
-		int i = 0;
-		int min = Integer.MAX_VALUE;
-		while (i < size){
-			int j = i;
-			int count = 1;
-			int sum = 0;
-			while (count < size){
-				sum += arr.get(j);
-				if (sum == target){
-					int moveCount = getMoveCount(i, count);
-					min = Math.min(min, moveCount);
-					break;
+		public int solution(int[] queue1, int[] queue2) {
+			int answer = 0;
+			q1 = queue1;
+			q2 = queue2;
+			q1Size = q1.length;
+			q2Size = q2.length;
+			end = q1Size;
+			target += Arrays.stream(queue1).reduce(0, (a, b) -> a + b);
+			target += Arrays.stream(queue2).reduce(0, (a, b) -> a + b);
+			if (target % 2 == 1)
+				return -1;
+			target /= 2;
+			while (true){
+				tmp = sumArr(start, end);
+				if (tmp > target){
+					start++;
+					answer++;
 				}
-				else if (sum > target){
-					break;
+				else if (tmp < target){
+					end++;
+					answer++;
 				}
-				j++;
-				count++;
-				if (j == size){
-					j = 0;
+				else{
+					return answer;
 				}
+				// start end 가 인덱스를 초기화 하는 경우에 대한 처리가 필요
 			}
 
-			i++;
+			return answer;
 		}
-		if (min == Integer.MAX_VALUE){
-			return -1;
+
+		private int sumArr(int start, int end){
+			int result = 0;
+
 		}
-		return min;
 	}
 
 }
