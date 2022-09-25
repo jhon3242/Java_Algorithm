3c1243b4b4e9d5d808da4c49672cae8bf97fc2ff
jhon3242 <rldotnr2@gmail.com>
2022-09-25 14:13:40 +0900

    [feat] | programmers 2개 이하로 다른 비트 77885 성공 https://school.programmers.co.kr/learn/courses/30/lessons/77885 생각해보니 간단하게 2진수에서 가장 마지막에 오는 0을 1로 바꿔주고 그 이후로 오는 가장 먼저 오는 1을 0으로 바꿔주면 되었다. 예를 들어서 119 의 경우 2진수가 1110111 인데 1111011 와 같이 바꿔주면 2개만 바뀐 최소 숫자가 된다.

--- src/programmers/level2/p77885/Solution.java
+++ src/programmers/level2/p77885/Solution.java
@@ -1,40 +1,52 @@
 package programmers.level2.p77885;
 
-import java.util.*;
-
 class Solution {
-	private static String target;
-
 	public long[] solution(long[] numbers) {
 		long[] answer = new long[numbers.length];
 
 		for (int i=0; i<numbers.length; i++){
-			long cur = numbers[i];
-
-			target = Long.toString(cur, 2);
-			int k = 1;
-			while (!oneOrTwo(cur + k)){
-				k++;
+			String bit = Long.toString(numbers[i], 2);
+			char[] target = bit.toCharArray();
+			char[] newBits;
+			int zeroIdx = -1, startIdx, changeIdx, count;
+			for (int j=target.length-1; j>=0; j--){
+				if (target[j] == '0'){
+					zeroIdx = j;
+					break;
+				}
 			}
-			answer[i] = cur + k;
+			if (zeroIdx == -1){ // 1111 같이 1만 있는 2진수의 경우
+				newBits = new char[target.length + 1];
+				newBits[0] = '1';
+				startIdx = 1;
+				changeIdx = 1;
+				count = 1;
+			}
+			else{
+				newBits = new char[target.length];
+				newBits[zeroIdx] = '1';
+				startIdx = 0;
+				changeIdx = zeroIdx + 1;
+				count = 2;
+			}
+
+			// copy
+			for (int j=0; j<target.length; j++){
+				if (j != zeroIdx)
+					newBits[j + startIdx] = target[j];
+			}
+
+			// 0 바꾸기
+			for (int j=changeIdx; j<newBits.length; j++){
+				if (newBits[j] == '1'){
+					newBits[j] = '0';
+					count--;
+				}
+				if (count >= 0)
+					break;
+			}
+			answer[i] = Long.parseLong(new String(newBits), 2);
 		}
 		return answer;
-	}
-
-	private boolean oneOrTwo(long num){
-		String bit = Long.toString(num, 2);
-		int count = 0;
-
-		if (target.length() < bit.length()){
-			target = "0" + target;
-		}
-		for (int i=bit.length() -1; i>=0; i--){
-			if (bit.charAt(i) != target.charAt(i)){
-				count++;
-			}
-			if (count > 2)
-				return false;
-		}
-		return true;
 	}
 }
