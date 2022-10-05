0575c1a8ce10d8e330044f83bc0a3c9b12df4d32
jhon3242 <rldotnr2@gmail.com>
2022-10-05 17:04:21 +0900

    [fix] | programmers 두 큐 합 같게 만들기 118667 성공 https://school.programmers.co.kr/learn/courses/30/lessons/118667 stream 을 사용하여 배열 원소의 합을 구했었는데 for 문으로 바꾸니까 바로 통과했다.. 왜 둘의 결과가 다른지는 모르겠다. 속도면에서 차이가 날 수는 있지만 시간초과로 틀린게 아니라 그냥 실패로 나온걸로 봐서는 예외 케이스가 존재하는 것 같은데,,

--- src/programmers/level2/p118667/Solution2.java
+++ src/programmers/level2/p118667/Solution2.java
@@ -18,27 +18,26 @@ class Solution {
 		concat = new int[q1Size + q2Size];
 		for (int i = 0; i < q1Size; i++) {
 			concat[i] = queue1[i];
+			target += queue1[i];
+			tmp += queue1[i];
 		}
 		for (int i = 0; i < q2Size; i++) {
 			concat[i + q1Size] = queue2[i];
+			target += queue2[i];
 		}
 		end = q1Size;
-		target = Arrays.stream(concat).reduce(0, (a, b) -> a + b);
 		if (target % 2 == 1)
 			return -1;
 
 		target /= 2;
-		// System.out.println("target " + target);
-		tmp = Arrays.stream(q1).reduce(0, (a, b) -> a + b);
-		while (answer < q1Size * 3){
+
+		while (start != end && answer <= q1Size * 3){
 			if (tmp > target){
-				// System.out.println("too big");
 				tmp -= concat[start % (q1Size + q2Size)];
 				start++;
 				answer++;
 			}
 			else if (tmp < target){
-				// System.out.println("too small");
 				tmp += concat[end % (q1Size + q2Size)];
 				end++;
 				answer++;
@@ -46,35 +45,9 @@ class Solution {
 			else{
 				return answer;
 			}
-			if (start >= q1Size + q2Size){
-				start -= q1Size + q2Size;
-			}
-			if (end >= q1Size + q2Size){
-				end -= q1Size + q2Size;
-			}
+			start %= q1Size + q2Size;
+			end %= q1Size + q2Size;
 		}
 		return -1;
-	}
-}
-
-public class Solution2 {
-
-	public static void main(String[] args) {
-		Solution solution = new Solution();
-//		int[] q1 = {3, 2, 7, 2};
-//		int[] q2 = {4, 6, 5, 1};
-//
-//		int[] q1 = {1, 2, 1, 2};
-//		int[] q2 = {1, 10, 1, 2};
-//
-//		int[] q1 = {1, 5};
-//		int[] q2 = {1, 1};
-
-		int[] q1 = {3, 2, 4};
-		int[] q2 = {1, 15, 5};
-
-
-		int solution1 = solution.solution(q1, q2);
-		System.out.println("solution1 = " + solution1);
 	}
 }
