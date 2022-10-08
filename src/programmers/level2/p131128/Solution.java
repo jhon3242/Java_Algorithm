e0480a68dc8f3460e8be9ebaaf608ced6dc8ced0
jhon3242 <rldotnr2@gmail.com>
2022-10-08 21:42:22 +0900

    [feat] | programmers 숫자 짝꿍 131128 성공 https://school.programmers.co.kr/learn/courses/30/lessons/131128 해쉬맵 2개를 만들어서 두 문자열의 숫자가 올 떄 마다 1씩 증가시켜서 숫자의 개수를 파악하는 방식으로 구현하여 둘 중 더 작은 횟수만큼 문자를 추가해주었는데 이때 반복문으로 추가해주었더니 시간 초과가 발생했다. 그래서 repeat 라는 함수를 찾아서 사용해주었는데 바로 통과됐다.

---
+++ src/programmers/level2/p131128/Solution.java
@@ -0,0 +1,38 @@
+package programmers.level2.p131128;
+
+import java.util.*;
+
+class Solution {
+	public String solution(String X, String Y) {
+		String answer = "";
+		Map<Character, Integer> xStore = new HashMap<>();
+		Map<Character, Integer> yStore = new HashMap<>();
+
+		for (int i=0; i<10; i++){
+			char t = (char)(i + '0');
+			xStore.put(t, 0);
+			yStore.put(t, 0);
+		}
+
+		for (int i=0; i<X.length(); i++){
+			char tmp = X.charAt(i);
+			xStore.put(tmp, xStore.get(tmp) + 1);
+		}
+
+		for (int i=0; i<Y.length(); i++){
+			char tmp = Y.charAt(i);
+			yStore.put(tmp, yStore.get(tmp) + 1);
+		}
+
+		for (int i=9; i>=0; i--){
+			char num = (char)(i + '0');
+			int time = Math.min(xStore.get(num), yStore.get(num));
+			answer += String.valueOf(num).repeat(time);
+		}
+		if (answer.length() == 0)
+			return "-1";
+		if (answer.charAt(0) == '0')
+			return "0";
+		return answer;
+	}
+}
