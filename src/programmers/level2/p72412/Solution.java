61640dcea2b76e3a1c4da777741c9cc58094860d
jhon3242 <rldotnr2@gmail.com>
2022-10-10 01:06:36 +0900

    [fix] | programmers 순위 검색 72412 실패 40 https://school.programmers.co.kr/learn/courses/30/lessons/72412 코드를 깔끔하게 다시 짰는데 정확성은 다 맞았지만 효율성에서 다 틀렸다.

--- src/programmers/level2/p72412/Solution.java
+++ src/programmers/level2/p72412/Solution.java
@@ -2,65 +2,80 @@ package programmers.level2.p72412;
 
 import java.util.*;
 
-class Candidate implements Comparable<Candidate>{
-	String language, part, career, food;
-	int score;
-
-	Candidate(String language, String part, String career, String food, int score){
-		this.language = language;
-		this.part = part;
-		this.career = career;
-		this.food = food;
-		this.score = score;
-	}
-
-	@Override
-	public int compareTo(Candidate can){
-		if (this.score < can.score){
-			return 1;
-		}
-		else
-			return -1;
-	}
-}
-
 class Solution {
+	Map<String, LinkedList> store = new HashMap<>();
 	public int[] solution(String[] info, String[] query) {
 		int[] answer = new int[query.length];
-		List<Candidate> list = new ArrayList<>();
 
-		for (String in : info){
-			String[] splited = in.split(" ");
-			list.add(new Candidate(splited[0], splited[1], splited[2], splited[3], Integer.parseInt(splited[4])));
+		store.put("python", new LinkedList<>());
+		store.put("cpp", new LinkedList<>());
+		store.put("java", new LinkedList<>());
+
+		for(String in : info){
+			String[] split = in.split(" ");
+			int part = split[1].equals("backend") ? 1 : 2;
+			int career = split[2].equals("junior") ? 1 : 2;
+			int food = split[3].equals("chicken") ? 1 : 2;
+			int score = Integer.parseInt(split[4]);
+			store.get(split[0]).add(new int[]{part, career, food, score});
 		}
-		Collections.sort(list);
 
-		for (int i=0; i<query.length; i++){
-			String[] splited = query[i].split(" ");
-			int minScore = Integer.parseInt(splited[7]);
-			int count = 0;
-			// System.out.println(i);
-			for (Candidate can : list){
-				// System.out.println(can.language + " " + can.part + " " + can.career + " " + can.food);
-				// System.out.println("score ck");
-				if (can.score < minScore)
-					break;
-				// System.out.println("len ck");
-				if (!splited[0].equals("-") && !can.language.equals(splited[0]))
-					continue;
-				// System.out.println("part ck " + can.part + " " + splited[2]);
-				if (!splited[2].equals("-") && !can.part.equals(splited[2]))
-					continue;
-				// System.out.println("career ck");
-				if (!splited[4].equals("-") && !can.career.equals(splited[4]))
-					continue;
-				// System.out.println("food ck");
-				if (!splited[6].equals("-") && !can.food.equals(splited[6]))
-					continue;
-				count++;
+		Collections.sort(store.get("python"), new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o2[3] - o1[3];
 			}
-			answer[i] = count;
+		});
+		Collections.sort(store.get("cpp"), new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o2[3] - o1[3];
+			}
+		});
+		Collections.sort(store.get("java"), new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o2[3] - o1[3];
+			}
+		});
+		int idx = 0;
+		for(String q : query){
+			String[] split = q.split(" ");
+			String lan = split[0];
+			int part = split[2].equals("backend") ? 1 : split[2].equals("frontend") ? 2 : 0;
+			int career = split[4].equals("junior") ? 1 : split[4].equals("senior") ? 2 : 0;
+			int food = split[6].equals("chicken") ? 1 : split[6].equals("pizza") ? 2 : 0;
+			int score = Integer.parseInt(split[7]);
+
+			int[] target = new int[]{part, career, food, score};
+			// all
+			if (lan.equals("-")){
+				answer[idx] += getMatchCount(store.get("python"), target);
+				answer[idx] += getMatchCount(store.get("cpp"), target);
+				answer[idx] += getMatchCount(store.get("java"), target);
+			}
+			else{
+				answer[idx] += getMatchCount(store.get(lan), target);
+			}
+			idx++;
 		}
+
 		return answer;
+	}
+
+	private int getMatchCount(LinkedList<int[]> list, int[] target){
+		int count = 0;
+		for (int[] cur : list){
+			if (cur[3] < target[3])
+				return count;
+			if (target[0] != 0 && cur[0] != target[0])
+				continue;
+			if (target[1] != 0 && cur[1] != target[1])
+				continue;
+			if (target[2] != 0 && cur[2] != target[2])
+				continue;
+			count++;
+		}
+		return count;
 	}
 }
