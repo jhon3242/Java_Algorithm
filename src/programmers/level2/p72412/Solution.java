ca5a4101745c82c43976ed94b98e9e640ad702bb
jhon3242 <rldotnr2@gmail.com>
2022-10-10 14:38:15 +0900

    [fix] | programmers 순위 검색 72412 실패 40 https://school.programmers.co.kr/learn/courses/30/lessons/72412 점수 탐색을 이진 탐색으로 바꾸었지만 여전히 시간 초과가 발생했다. 아마도 info 를 추가하는 부분에서 반복문을 여러번 사용해서 시간 초과가 나는것 같다.

--- src/programmers/level2/p72412/Solution.java
+++ src/programmers/level2/p72412/Solution.java
@@ -7,75 +7,78 @@ class Solution {
 	public int[] solution(String[] info, String[] query) {
 		int[] answer = new int[query.length];
 
-		store.put("python", new LinkedList<>());
-		store.put("cpp", new LinkedList<>());
-		store.put("java", new LinkedList<>());
+		for (String lan : new String[]{"python", "cpp", "java", "-"}){
+			StringBuilder sb = new StringBuilder();
+			sb.append(lan);
+			for(String part : new String[]{"backend", "frontend", "-"}){
+				sb.append(part);
+				for(String career : new String[]{"junior", "senior", "-"}){
+					sb.append(career);
+					for(String food : new String[]{"chicken", "pizza", "-"}){
+						sb.append(food);
+						// System.out.println("#" + sb + "#");
+						store.put(sb.toString(), new LinkedList<>());
+						sb.setLength(sb.length() - food.length());
+					}
+					sb.setLength(sb.length() - career.length());
+				}
+				sb.setLength(sb.length() - part.length());
+			}
+			sb.setLength(sb.length() - lan.length());
+		}
+		// for(String k : store.keySet()){
+		// 	System.out.println("k = " + k);
+		// }
 
-		for(String in : info){
-			String[] split = in.split(" ");
-			int part = split[1].equals("backend") ? 1 : 2;
-			int career = split[2].equals("junior") ? 1 : 2;
-			int food = split[3].equals("chicken") ? 1 : 2;
-			int score = Integer.parseInt(split[4]);
-			store.get(split[0]).add(new int[]{part, career, food, score});
+		for (String i : info){
+			String[] split = i.split(" ");
+			for (String lan : new String[]{split[0], "-"}){
+				StringBuilder sb = new StringBuilder();
+				sb.append(lan);
+				for(String part : new String[]{split[1], "-"}){
+					sb.append(part);
+					for(String career : new String[]{split[2], "-"}){
+						sb.append(career);
+						for(String food : new String[]{split[3], "-"}){
+							sb.append(food);
+							store.get(sb.toString()).add(Integer.parseInt(split[4]));
+							sb.setLength(sb.length() - food.length());
+						}
+						sb.setLength(sb.length() - career.length());
+					}
+					sb.setLength(sb.length() - part.length());
+				}
+				sb.setLength(sb.length() - lan.length());
+			}
 		}
 
-		Collections.sort(store.get("python"), new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o2[3] - o1[3];
-			}
-		});
-		Collections.sort(store.get("cpp"), new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o2[3] - o1[3];
-			}
-		});
-		Collections.sort(store.get("java"), new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o2[3] - o1[3];
-			}
-		});
+		for(String k : store.keySet()){
+			Collections.sort(store.get(k), Collections.reverseOrder());
+		 }
+
 		int idx = 0;
-		for(String q : query){
+		for (String q : query){
 			String[] split = q.split(" ");
-			String lan = split[0];
-			int part = split[2].equals("backend") ? 1 : split[2].equals("frontend") ? 2 : 0;
-			int career = split[4].equals("junior") ? 1 : split[4].equals("senior") ? 2 : 0;
-			int food = split[6].equals("chicken") ? 1 : split[6].equals("pizza") ? 2 : 0;
+			String curKey = split[0] + split[2] + split[4] + split[6];
 			int score = Integer.parseInt(split[7]);
 
-			int[] target = new int[]{part, career, food, score};
-			// all
-			if (lan.equals("-")){
-				answer[idx] += getMatchCount(store.get("python"), target);
-				answer[idx] += getMatchCount(store.get("cpp"), target);
-				answer[idx] += getMatchCount(store.get("java"), target);
-			}
-			else{
-				answer[idx] += getMatchCount(store.get(lan), target);
-			}
-			idx++;
+			LinkedList<Integer> list = store.get(curKey);
+			int count = getCount(list, score);
+			System.out.println("count = " + count);
+			answer[idx++] = count;
 		}
-
 		return answer;
 	}
 
-	private int getMatchCount(LinkedList<int[]> list, int[] target){
-		int count = 0;
-		for (int[] cur : list){
-			if (cur[3] < target[3])
-				return count;
-			if (target[0] != 0 && cur[0] != target[0])
-				continue;
-			if (target[1] != 0 && cur[1] != target[1])
-				continue;
-			if (target[2] != 0 && cur[2] != target[2])
-				continue;
-			count++;
+	private int getCount(LinkedList<Integer> list, int score){
+		int start = 0, end = list.size() - 1;
+		while (start <= end){
+			int mid = (start + end) / 2;
+			if (list.get(mid) < score) // 오른쪽에 있는 경우
+				start = mid + 1;
+			else
+				end = mid - 1;
 		}
-		return count;
+		return list.size() - start;
 	}
 }
