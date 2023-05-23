0661b8624cb329d864105ce1f06cd57b199229e0
jhon3242 <rldotnr2@gmail.com>
2023-05-23 14:24:46 +0900

    feat (baekjoon) : 1916 문제풀이 추가 3차 시도 (성공) 다익스트라 알고리즘을 통해 성공했다.

--- src/baekjoon/p1916/Main.java
+++ src/baekjoon/p1916/Main.java
@@ -3,48 +3,82 @@ package baekjoon.p1916;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class Main {
 
-	private static int[][] graph;
+	private static List<Node>[] list;
+	private static int N;
+	private static int M;
+	private static int[] distance;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
 
-		graph = new int[N + 1][N + 1];
+		list = new ArrayList[N + 1];
+		distance = new int[N + 1];
 
-		for (int i = 0; i <= N; i++) {
-			Arrays.fill(graph[i], 200_000);
-		}
+		Arrays.fill(distance, Integer.MAX_VALUE);
 
 		for (int i = 1; i <= N; i++) {
-			graph[i][i] = 0;
+			list[i] = new ArrayList<>();
 		}
 
 		for (int i = 0; i < M; i++) {
 			int[] in = Arrays.stream(br.readLine().split(" "))
 					.mapToInt(Integer::parseInt)
 					.toArray();
-			graph[in[0]][in[1]] = Math.min(graph[in[0]][in[1]], in[2]);
+			list[in[0]].add(new Node(in[1], in[2]));
 		}
 
 		int[] root = Arrays.stream(br.readLine().split(" "))
 				.mapToInt(Integer::parseInt)
 				.toArray();
 
-		for (int k = 1; k <= N; k++) {
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
-					graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+		int start = root[0];
+		int end = root[1];
+		dijkstra(start);
+		System.out.println(distance[end]);
+	}
+
+	private static void dijkstra(int start) {
+		PriorityQueue<Node> q = new PriorityQueue<>();
+		boolean[] visited = new boolean[N + 1];
+		q.add(new Node(start, 0));
+		distance[start] = 0;
+		while (!q.isEmpty()) {
+			Node poll = q.poll();
+			int cur = poll.end;
+			if (visited[cur]) continue;
+			visited[cur] = true;
+			for (Node node : list[cur]) {
+				if (distance[node.end] > distance[cur] + node.cost) {
+					distance[node.end] = distance[cur] + node.cost;
+					q.add(new Node(node.end, distance[node.end]));
 				}
 			}
 		}
+	}
 
-		System.out.println(graph[root[0]][root[1]]);
+	static class Node implements Comparable<Node> {
+		private int end;
+		private int cost;
+
+		public Node(int end, int cost) {
+			this.end = end;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Node node) {
+			return this.cost - node.cost;
+		}
 	}
 }
 
