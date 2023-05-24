317721a6b25cb37140af2d0b18124f3aa624763f
jhon3242 <rldotnr2@gmail.com>
2023-05-24 15:14:27 +0900

    feat (baekjoon) : 2252 문제풀이 추가 2차 시도 (성공) 위상정렬로 문제 풀이 성공. 처음에는 boolean 2차원 배열로 경로를 설정했으나 메모리 초과가 발생했었다. 메모리 여유가 있어도 무작정 2차원 배열로 만드는 것보다는 List 로 만드는게 더 효율적일듯 하다.

--- src/baekjoon/p2252/Main.java
+++ src/baekjoon/p2252/Main.java
@@ -1,50 +1,107 @@
 package baekjoon.p2252;
 
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class Main {
-	static int N, M;
-	static List<Integer>[] list;
-	static int[] indegree;
+	private static int N;
+	private static int M;
+	private static List<Integer>[] graph;
+	private static int[] indegree;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		Queue<Integer> queue = new LinkedList<>();
+		Queue<Integer> qu = new LinkedList<>();
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		list = new List[N + 1];
+		graph = new ArrayList[N + 1];
 		indegree = new int[N + 1];
+
 		for (int i = 0; i <= N; i++) {
-			list[i] = new ArrayList<>();
+			graph[i] = new ArrayList<>();
 		}
 
-		// init road
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int shor = Integer.parseInt(st.nextToken());
-			int tall = Integer.parseInt(st.nextToken());
-			list[shor].add(tall);
-			indegree[tall]++;
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			graph[start].add(end);
+			indegree[end]++;
 		}
 
-		for (int i = 1; i < indegree.length; i++) {
-			if (indegree[i] == 0){
-				queue.add(i);
-				System.out.print(i + " ");
+		for (int i = 1; i <= N; i++) {
+			if (indegree[i] == 0) {
+				qu.add(i);
 			}
 		}
 
-		while (!queue.isEmpty()){
-			Integer cur = queue.poll();
-			for (int i : list[cur]){
-				indegree[i]--;
-				if (indegree[i] == 0){
-					queue.add(i);
-					System.out.print(i + " ");
-				}
+		while (!qu.isEmpty()) {
+			int poll = qu.poll();
+			System.out.print(poll + " ");
+			for (int i : graph[poll]) {
+					indegree[i]--;
+					if (indegree[i] == 0) {
+						qu.add(i);
+					}
 			}
 		}
+		System.out.println();
 	}
 }
+
+
+//import java.util.*;
+//import java.io.*;
+//
+//public class Main {
+//	static int N, M;
+//	static List<Integer>[] list;
+//	static int[] indegree;
+//
+//	public static void main(String[] args) throws IOException{
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		Queue<Integer> queue = new LinkedList<>();
+//		N = Integer.parseInt(st.nextToken());
+//		M = Integer.parseInt(st.nextToken());
+//		list = new List[N + 1];
+//		indegree = new int[N + 1];
+//		for (int i = 0; i <= N; i++) {
+//			list[i] = new ArrayList<>();
+//		}
+//
+//		// init road
+//		for (int i = 0; i < M; i++) {
+//			st = new StringTokenizer(br.readLine());
+//			int shor = Integer.parseInt(st.nextToken());
+//			int tall = Integer.parseInt(st.nextToken());
+//			list[shor].add(tall);
+//			indegree[tall]++;
+//		}
+//
+//		for (int i = 1; i < indegree.length; i++) {
+//			if (indegree[i] == 0){
+//				queue.add(i);
+//				System.out.print(i + " ");
+//			}
+//		}
+//
+//		while (!queue.isEmpty()){
+//			Integer cur = queue.poll();
+//			for (int i : list[cur]){
+//				indegree[i]--;
+//				if (indegree[i] == 0){
+//					queue.add(i);
+//					System.out.print(i + " ");
+//				}
+//			}
+//		}
+//	}
+//}
