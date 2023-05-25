074383c663afa1f846b1ae289e88a40996d0f4e5
jhon3242 <rldotnr2@gmail.com>
2023-05-25 12:14:17 +0900

    feat (baekjoon) : 1789 문제풀이 추가 2차 시도 (성공) 차근히 예제를 적어보면서 하면 쉽게 규칙이 보이는 문제였다. 숫자 타입을 long 으로 바꿔줘야하기 때문에 parseLong 을 써야했다.

--- src/baekjoon/p1789/Main.java
+++ src/baekjoon/p1789/Main.java
@@ -1,5 +1,6 @@
 package baekjoon.p1789;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,15 +8,37 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		long S = Long.parseLong(br.readLine());
-		long n = 1;
-		while (S >= sumN(n)){
-			n++;
-		}
-		System.out.println(n - 1);
-	}
 
-	public static long sumN(long n){
-		return (n * (n + 1)) / 2;
+		long S = Long.parseLong(br.readLine());
+
+		long result = 0;
+		for (long i = 1; i < Integer.MAX_VALUE; i++) {
+			if (i * (i + 1) / 2 > S) {
+				System.out.println(result);
+				return;
+			}
+			result++;
+		}
 	}
 }
+
+
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//
+//public class Main {
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		long S = Long.parseLong(br.readLine());
+//		long n = 1;
+//		while (S >= sumN(n)){
+//			n++;
+//		}
+//		System.out.println(n - 1);
+//	}
+//
+//	public static long sumN(long n){
+//		return (n * (n + 1)) / 2;
+//	}
+//}
