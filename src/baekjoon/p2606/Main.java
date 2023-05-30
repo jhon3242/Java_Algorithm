5a26de6dd5c274f6cef4160f0e8501f02f718701
jhon3242 <rldotnr2@gmail.com>
2023-05-30 12:56:32 +0900

    feat (baekjoon) : 2606 문제풀이 추가 1차 시도 (성공) DFS 로 푸니까 바로 풀렸다.

--- src/baekjoon/p2606/Main.java
+++ src/baekjoon/p2606/Main.java
@@ -4,49 +4,53 @@ package baekjoon.p2606;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	private static int N;
-	private static int[] parents;
+	private static boolean[][] graph;
+	private static boolean[] visited;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		N = Integer.parseInt(br.readLine());
-		parents = new int[N + 1];
+		graph = new boolean[N + 1][N + 1];
+		visited = new boolean[N + 1];
 		int k = Integer.parseInt(br.readLine());
-
-		for (int i = 1; i <= N; i++) {
-			parents[i] = i;
-		}
 
 		for (int i = 0; i < k; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			if (findParents(a) != findParents(b)) {
-				unionParents(a, b);
+			graph[a][b] = true;
+			graph[b][a] = true;
+		}
+
+		bfs();
+	}
+
+	private static void bfs() {
+		Queue<Integer> q = new LinkedList<>();
+		int count = -1; // 1 번 컴퓨터는 카운트 되면 안되기 때문에 -1 로 시작
+
+		q.add(1);
+		while (!q.isEmpty()) {
+			Integer node = q.poll();
+			if (!visited[node]) {
+				visited[node] = true;
+				count++;
+
+				for (int i = 1; i <= N; i++) {
+					if (!visited[i] && graph[node][i]) {
+						q.add(i);
+					}
+				}
 			}
 		}
-
-		int result = 0;
-		for (int i = 2; i <= N; i++) {
-			if (findParents(i) == findParents(1)) result++;
-		}
-		System.out.println(result);
-	}
-
-	private static int findParents(int x) {
-		if (parents[x] != x) parents[x] = findParents(parents[x]);
-		return parents[x];
-	}
-
-	private static void unionParents(int a, int b) {
-		int aR = findParents(a);
-		int bR = findParents(b);
-		if (aR < bR) parents[b] = aR;
-		else parents[a] = bR;
+		System.out.println(count);
 	}
 }
