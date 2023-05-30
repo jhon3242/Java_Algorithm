d08dff911c141873ad0cb43038e8d0c82e535e80
jhon3242 <rldotnr2@gmail.com>
2023-05-30 15:08:30 +0900

    feat (baekjoon) : 12851 문제풀이 추가 2차 시도 (실패) DFS 로 풀어보려고 했으나 +1 로 너무 많은 재귀가 일어나서 실패했다.

--- src/baekjoon/p12851/Main.java
+++ src/baekjoon/p12851/Main.java
@@ -3,14 +3,13 @@ package baekjoon.p12851;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	private static int N;
 	private static int K;
-	private static int sameCount = 0;
 	private static int[] dp = new int[100_001];
+	private static int sameCount = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,54 +18,32 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-
-		bfs();
-		System.out.println(dp[K] - 1);
-		System.out.println(sameCount);
+		dfs(N, 1);
 	}
 
-	private static void bfs() {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-
-		pq.add(new Node(N, 1));
-		while (!pq.isEmpty()) {
-			Node poll = pq.poll();
-			if (poll.cur < 0 || poll.cur > 100_000) continue;
-			if (dp[poll.cur] != 0 && poll.count > dp[poll.cur]) continue;
-
-			if (poll.cur == K) {
-				if (dp[poll.cur] == 0 || dp[poll.cur] > poll.count) {
-					dp[poll.cur] = poll.count;
-					sameCount = 1;
-					continue;
-				}
-				if (poll.count > dp[poll.cur]) return;
+	private static void dfs(int cur, int count) {
+		if (cur == K) {
+			if (dp[cur] > count) {
+				dp[cur] = count;
+				sameCount = 1;
+			}
+			if (dp[cur] == count) {
 				sameCount++;
-				continue;
 			}
-
-			if (dp[poll.cur] == 0 || dp[poll.cur] > poll.count) {
-				dp[poll.cur] = poll.count;
-				pq.add(new Node(poll.cur + 1, poll.count + 1));
-				pq.add(new Node(poll.cur - 1, poll.count + 1));
-				pq.add(new Node(poll.cur * 2, poll.count + 1));
-			}
+			return;
 		}
 
+		if (cur < 0 || cur > 100_000) return;
+		if (dp[cur] != 0 && dp[cur] <= count) return;
+
+		dp[cur] = count;
+		System.out.println("cur = " + cur + " count " + count);
+
+		dfs(cur * 2, count + 1);
+		dfs(cur + 1, count + 1);
+		dfs(cur - 1, count + 1);
 	}
 
-	static class Node implements Comparable<Node> {
-		int cur;
-		int count;
 
-		public Node(int cur, int count) {
-			this.cur = cur;
-			this.count = count;
-		}
 
-		@Override
-		public int compareTo(Node o) {
-			return count - o.count;
-		}
-	}
 }
