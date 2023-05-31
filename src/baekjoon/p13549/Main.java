18e939501e0529ad0c450b4dea0ee917714810b4
jhon3242 <rldotnr2@gmail.com>
2023-05-31 12:14:55 +0900

    feat (baekjoon) : 13549 문제풀이 추가 1차 시도 (성공) 숨박꼭질 3 문제로 12851 문제랑 매우 유사한 문제였다. 다른 점은 순간이동시에 시간이 0초가 소비되는 걸로 바뀌어 dp 배열의 현재값과 일치하는 경우에도 큐에 추가해주면 0 인 경우가 계속 추가되어 메모리 초과가 발생하므로 dp 배열의 현재값보다 작은 경우에만 큐에 추가하는 방식으로 바꿔주었다. 이렇게 되면 이전 12851 문제에서 처럼 1 -> 2 -> 4 의 경우 하나의 경우의 수만 추가되는데 이번에는 1 -> 1 + 1 -> 4 와 1 -> 1 * 2 -> 4 의 시간경과가 각각 1, 2 이므로 다른 경우이기 때문에 문제가 없다.

---
+++ src/baekjoon/p13549/Main.java
@@ -0,0 +1,73 @@
+package baekjoon.p13549;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static int N;
+	private static int K;
+	private static int[] dp = new int[100_001];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		Arrays.fill(dp, -1);
+		bfs();
+
+		System.out.println(dp[K]);
+	}
+
+	private static void bfs() {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+
+		pq.add(new Node(N, 0));
+		while (!pq.isEmpty()) {
+			Node poll = pq.poll();
+			// 유효하지 않는 값이면 넘기기
+			if (poll.pos < 0 || poll.pos > 100000) continue;
+
+			// 더이상 더 작은 경우가 안나오는 경우 종료
+			if (dp[K] != -1 && dp[K] < poll.count) return;
+
+			if (poll.pos == K) {
+				// 값이 바뀌는 경우 업데이트
+				if (dp[K] == -1 || dp[K] > poll.count) {
+					dp[K] = poll.count;
+					return;
+				}
+			}
+
+			if (dp[poll.pos] == -1 || dp[poll.pos] > poll.count) {
+				dp[poll.pos] = poll.count;
+				pq.add(new Node(poll.pos + 1, poll.count + 1));
+				pq.add(new Node(poll.pos - 1, poll.count + 1));
+				pq.add(new Node(poll.pos * 2, poll.count));
+			}
+		}
+	}
+
+
+	static class Node implements Comparable<Node> {
+		int pos;
+		int count;
+
+		public Node(int pos, int count) {
+			this.pos = pos;
+			this.count = count;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return count - o.count;
+		}
+	}
+}
+
