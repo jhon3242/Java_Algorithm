2bd3a5688c0b97b022cc2257f1a698b98c777f68
jhon3242 <rldotnr2@gmail.com>
2023-06-05 13:15:24 +0900

    feat (baekjoon) : 1890 문제풀이 추가 1차 시도 (성공) 2차원 그래프를 하나더 만들고 현재 위치에서 경로가 총 몇게 있는지를 저장하는 방식으로 DP 를 사용했다. 또한 N 이 10 일 떄 결과가 985525432 로 매우 큰 것을 보고 오버플로우가 나겠다 싶어서 long 으로 타입을 바꿔줬다.

--- src/baekjoon/p1890/Main.java
+++ src/baekjoon/p1890/Main.java
@@ -8,31 +8,36 @@ import java.util.Arrays;
 public class Main {
 	private static int N;
 	private static int[][] graph;
-	private static long result = 0;
+	private static long[][] resultG;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		graph = new int[N][N];
+		resultG = new long[N][N];
 
 		for (int i = 0; i < N; i++) {
 			graph[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 		}
 
-		dfs(0, 0);
+		long result = dfs(0, 0);
 		System.out.println(result);
 
 	}
 
-	private static void dfs(int x, int y) {
+	private static long dfs(int x, int y) {
+		long count = 0;
+
+		if (resultG[x][y] != 0) {
+			return resultG[x][y];
+		}
 
 		if (x == N - 1 && y == N - 1) {
-			result++;
-			return;
+			return 1;
 		}
 
 		int moveCount = graph[x][y];
 		if (moveCount == 0) {
-			return;
+			return 0;
 		}
 
 		int tx = x + moveCount;
@@ -40,12 +45,15 @@ public class Main {
 
 		// right move
 		if (0 <= ty && ty < N) {
-			dfs(x, ty);
+			count += dfs(x, ty);
 		}
 
 		// down move
 		if (0 <= tx && tx < N) {
-			dfs(tx, y);
+			count += dfs(tx, y);
 		}
+		resultG[x][y] = count;
+
+		return count;
 	}
 }
