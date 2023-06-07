55a4eb9585f2312c3eed1cfa06b33b0455e931e6
jhon3242 <rldotnr2@gmail.com>
2023-06-07 11:33:09 +0900

    feat (baekjoon) : 12026 문제풀이 추가 2차 시도 (실패 : 예외케이스) BOJ 를 최대한 많이 만들어야 한다는 생각으로 접근하여 B와 J 의 인덱스를 찾고 가운데 인덱스부터 O 를 찾는 방식으로 구현하려고 했으나 두번째 B 의 인덱스를 찾을 때 또한 두번째 O 와 첫번째 J 의 사이에 있어야 한다는 것을 깨달았다.

--- src/baekjoon/p12026/Main.java
+++ src/baekjoon/p12026/Main.java
@@ -6,6 +6,12 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * BOOOOJOOOOOJBO
+ * J 를 먼저 찾는다.
+ *  없으면 가장 먼 O 를 찾는다.
+ * 찾은 J 의 인덱스와 B 사이에 중간에서 가까운 O 를 찾는다
+ */
 public class Main {
 	private static int N;
 	private static char[] graph;
@@ -20,23 +26,79 @@ public class Main {
 		map.put('O', 'J');
 		map.put('J', 'B');
 
-		dfs(0, 0, 'O');
-		if (result == Integer.MAX_VALUE) System.out.println(-1);
-		else System.out.println(result);
+		// 최대한 BOJ 를 많이 만드는게 제일 적게 가는 방법
+		int totalBOJ = getTotalBOJCount();
+		int bIdx = 0;
+		int oIdx = 0;
+		int jIdx = 0;
+
+		while (totalBOJ > 0) {
+			if (totalBOJ <= 3) {
+				if (totalBOJ == 1) {
+					bIdx = findChar(jIdx, 'B');
+					// not valid
+					if (bIdx != graph.length - 1) {
+						System.out.println(-1);
+						return;
+					}
+					result += Math.pow(oIdx - bIdx, 2);
+				}
+				if (totalBOJ == 2) {
+
+				}
+
+				break;
+			}
+
+			bIdx = findChar(jIdx, 'B');
+			jIdx = findChar(bIdx, 'J');
+			oIdx = findO(bIdx, jIdx);
+			result += Math.pow(oIdx - bIdx, 2);
+			result += Math.pow(jIdx - oIdx, 2);
+			totalBOJ -= 3;
+		}
+
+
+
+//		if (result == Integer.MAX_VALUE) System.out.println(-1);
+//		else System.out.println(result);
 
 	}
 
-	private static void dfs(int cur , int index, char target) {
-
-		if (index == N - 1) {
-			result = Math.min(result, cur);
-			return;
-		}
-
-		for (int i = index + 1; i < N; i++) {
+	private static int getTotalBOJCount() {
+		int result = 0;
+		char target = 'B';
+		for (int i = 0; i < graph.length; i++) {
 			if (graph[i] == target) {
-				dfs(cur + (int)Math.pow(i - index, 2), i, map.get(graph[i]));
+				target = map.get(target);
+				result++;
 			}
 		}
+		return result;
 	}
+
+	// 사이에 무조건 O 가 있다고 가정
+	private static int findO(int b, int j) {
+
+		int midL = (b + j) / 2;
+		int midR = ((b + j)) / 2 + 1;
+
+		while (b < j) {
+			if (graph[midL] == 'O') return midL;
+			if (graph[midR] == 'O') return midR;
+			midL--;
+			midR++;
+		}
+		return -1;
+	}
+
+	private static int findChar(int start, char chr) {
+		for (int i = start; i < graph.length ; i++) {
+			if (graph[i] == chr) return i;
+		}
+		return -1;
+	}
+
+
+
 }
