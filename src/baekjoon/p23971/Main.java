1070e31d884e594ebe98eb28455c77c01fa80c04
jhon3242 <rldotnr2@gmail.com>
2023-06-16 11:41:39 +0900

    feat (baekjoon) : 23971 ZOAC 4 문제풀이 추가 1차 시도 (성공) 너무나 쉬운 수학 문제였다. 무지성으로 코딩을 하는게 얼마나 비효율적인지 느낄 수 있었다.

--- src/baekjoon/p23971/Main.java
+++ src/baekjoon/p23971/Main.java
@@ -3,8 +3,6 @@ package baekjoon.p23971;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,8 +10,6 @@ public class Main {
 	private static int W;
 	private static int N;
 	private static int M;
-	private static int[][] graph;
-	private static int result = 1;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,45 +20,19 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		graph = new int[H][W];
-		graph[0][0] = 1;
-		bfs();
-		System.out.println(result);
-	}
-
-	private static void bfs() {
-		Queue<Node> qu = new LinkedList<>();
-		int[] dx = new int[]{0, N + 1, 0, -N - 1};
-		int[] dy = new int[]{M + 1, 0, -M - 1, 0};
-
-		qu.add(new Node(0, 0));
-		while (!qu.isEmpty()) {
-			Node poll = qu.poll();
-
-			for (int i = 0; i < 4; i++) {
-				int tx = dx[i] + poll.x;
-				int ty = dy[i] + poll.y;
-
-				if (tx < 0 || ty < 0 || tx >= H || ty >= W) {
-					continue;
-				}
-				if (graph[tx][ty] == 1) {
-					continue;
-				}
-				graph[tx][ty] = 1;
-				result++;
-				qu.add(new Node(tx, ty));
-			}
+		int i = 0;
+		int rowMax = 0;
+		while (i < H) {
+			i += 1 + N;
+			rowMax++;
 		}
-	}
 
-	static class Node {
-		public int x;
-		public int y;
-
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
+		i = 0;
+		int colMax = 0;
+		while (i < W) {
+			i += 1 + M;
+			colMax++;
 		}
+		System.out.println(rowMax * colMax);
 	}
 }
