d96d0b809644dee37725b8123be44dbe26dfc44a
jhon3242 <rldotnr2@gmail.com>
2023-06-18 18:47:04 +0900

    feat (programers) : 150369 (택배 배달과 수거하기) 문제풀이 추가 2차 시도 (실패 : 예외케이스) 조건 문제인지 테스트케이스 하나 때문에 계속 실패한다.

--- src/programmers/level2/p150369/Solution.java
+++ src/programmers/level2/p150369/Solution.java
@@ -2,71 +2,63 @@ package programmers.level2.p150369;
 
 import java.util.*;
 
-/**
- * 정답 확인
- *
- * 항상 최대값으로 배달을 가져가서 배달하고
- * 항상 최대값으로 픽업하는 것만 생각하면 되고 어디에 얼마나 배달할지에 대해선 중요하지 않는다는 것을
- * 깨달지 못해서 어렵게 접근한듯 하다.
- */
 class Solution {
 	public long solution(int cap, int n, int[] deliveries, int[] pickups) {
 		long answer = 0;
-		int del = -1;
-		int pic = -1;
-		int size = cap;
+		int dI = 0;
+		int pI = 0;
+		int curCap;
+		int removeCount = 0;
 
-		for (int i = n - 1; i >=0 ; i--) {
-			if (deliveries[i] != 0) {
-				del = i;
+		for (int i = n - 1 ; i >= 0 ; i--) {
+			if (deliveries[i] > 0) {
+				dI = i;
 				break;
 			}
 		}
 
-		for (int i = n - 1; i >=0 ; i --) {
-			if (pickups[i] != 0) {
-				pic = i;
+		for (int i = n - 1 ; i >= 0 ; i--) {
+			if (pickups[i] > 0) {
+				pI = i;
 				break;
 			}
 		}
 
-		// int i = 0;
 		while (true) {
-			if (del < 0 && pic < 0) break;
-			// System.out.println("add " + Math.max(del + 1, pic + 1));
-			answer += Math.max(del + 1, pic + 1) * 2;
-
+			curCap = cap;
+			if (pI < 0 && dI < 0) break;
+			answer += Math.max(pI + 1, dI + 1) * 2;
+			// while (dI >= 0) {
+			//     if (deliveries[dI] > 0) {
+			//         if (curCap == 0) break;
+			//         removeCount = Math.min(curCap, deliveries[dI]);
+			//         curCap -= removeCount;
+			//         deliveries[dI] -= removeCount;
+			//         continue;
+			//     }
+			//     dI--;
+			// }
 			while (true) {
-				// System.out.println("cap " + cap + " del " + del);
-				if (del < 0  || cap == 0 && deliveries[del] != 0 ) break;
-				int count = Math.min(cap, deliveries[del]);
-				cap -= count;
-				deliveries[del] -= count;
-
-				if (deliveries[del] == 0) {
-					del--;
+				if (dI < 0 || curCap == 0 && deliveries[dI] != 0) break;
+				removeCount = Math.min(curCap, deliveries[dI]);
+				curCap -= removeCount;
+				deliveries[dI] -= removeCount;
+				if (deliveries[dI] == 0) {
+					dI--;
 				}
 			}
-			cap = size;
+			curCap = cap;
 
 			while (true) {
-				// System.out.println("cap " + cap + " del " + del);
-				if (pic < 0  || cap == 0 && pickups[pic] != 0 ) break;
-				int count = Math.min(cap, pickups[pic]);
-				cap -= count;
-				pickups[pic] -= count;
-
-				if (pickups[pic] == 0) {
-					pic--;
+				if (pI < 0 || (curCap == 0 && pickups[pI] != 0)) break;
+				removeCount = Math.min(curCap, pickups[pI]);
+				curCap -= removeCount;
+				pickups[pI] -= removeCount;
+				if (pickups[pI] == 0) {
+					pI--;
 				}
 			}
 
-			// System.out.println("deliveries " + Arrays.toString(deliveries));
-			// System.out.println("pickups " + Arrays.toString(pickups));
-			// System.out.println();
-			cap = size;
-			// i++;
-			// if (i > 3) break;
 		}
 		return answer;
 	}
