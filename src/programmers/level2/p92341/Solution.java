36cbd30a9202eb6f703745a9df446cd621e11e91
jhon3242 <rldotnr2@gmail.com>
2023-06-19 13:14:47 +0900

    feat (programers) : 92341 (주차 요금 계산) 문제풀이 추가 2차 시도 (성공) Map 을 사용하고 Car 클래스를 만들어서 풀었다.

--- src/programmers/level2/p92341/Solution.java
+++ src/programmers/level2/p92341/Solution.java
@@ -2,93 +2,118 @@ package programmers.level2.p92341;
 
 import java.util.*;
 
-class Car implements Comparable<Car>{
-	int carNum;
-	int totalTime;
-	int lastTime;
-	boolean state;
+/**
+ System.out.println("x = " + x);
+ */
+class Solution {
 
-	Car(int carNum){
-		this.carNum = carNum;
-	}
+	private final int Time = 0;
+	private final int Num = 1;
+	private final int Status = 2;
+	private Map<String, Car> store = new HashMap<>();
 
-	void setLastTime(int lastTime){
-		this.lastTime = lastTime;
-	}
-
-	int getLastTime(){
-		return this.lastTime;
-	}
-
-	void addTotalTime(int num){
-		totalTime += num;
-	}
-
-	@Override
-	public int compareTo(Car o){
-		return carNum - o.carNum;
-	}
-}
-
-public class Solution {
 	public int[] solution(int[] fees, String[] records) {
-		List<Integer> answerList = new LinkedList<>();
-		// PriorityQueue<Car> q = new PriorityQueue<>(); // 우선순위 큐로 구현하면 탐색을 계속 N 만큼 해줘야 해서 비효율적이다.
-		Car[] carArr = new Car[10000];
-		final int basicTime = fees[0];
-		final int basicFee = fees[1];
-		final int unitTime = fees[2];
-		final int unitFee = fees[3];
-
-		for (String reco : records){
-			String[] splited = reco.split(" ");
-			String status = splited[2];
-			String time = splited[0];
-			int carNum = Integer.parseInt(splited[1]);
-
-			if (status.equals("IN")){
-				if (carArr[carNum] == null)
-					carArr[carNum] = new Car(carNum);
-				carArr[carNum].setLastTime(timeToNum(time));
-				carArr[carNum].state = true;
-			}
-			else{
-				carArr[carNum].addTotalTime(timeToNum(time) - carArr[carNum].getLastTime());
-				carArr[carNum].state = false;
-			}
+		for (String rec : records) {
+			String[] split = rec.split(" ");
+			handleRecord(split);
 		}
+		handle24();
 
-		for(int i = 0; i < 10000; i++){
-			if (carArr[i] == null)
-				continue;
-			if (carArr[i].state) // 마지막에 출차하지 않은 경우
-				carArr[i].addTotalTime(timeToNum("23:59") - carArr[i].getLastTime());
-			int curTime = carArr[i].totalTime;
-
-			int result = basicFee;
-			if (curTime > basicTime){
-				int diff = curTime - basicTime;
-				if (diff % unitTime != 0){
-					diff /= unitTime;
-					diff += 1;
-				}
-				else{
-					diff /= unitTime;
-				}
-				result += diff * unitFee;
-			}
-			answerList.add(result);
+		for (Car tmp : store.values()) {
+			tmp.calculate(fees);
+			// System.out.println("tmp = " + tmp);
 		}
-
-		int[] answer = answerList.stream()
-				.mapToInt(i -> i)
-				.toArray();
-
-		return answer;
+		List<Car> cars = new ArrayList<>(store.values());
+		Collections.sort(cars);
+		return cars.stream().mapToInt(v -> v.totalFee).toArray();
 	}
 
-	private int timeToNum(String time){
-		String[] tmp = time.split(":");
-		return Integer.parseInt(tmp[0]) * 60 + Integer.parseInt(tmp[1]);
+	private void handleRecord(String[] split) {
+		Car tmpCar = store.getOrDefault(split[Num], new Car(split));
+		// 입차
+		if (split[Status].equals("IN")) {
+			tmpCar.handleIn(split[Time]);
+			store.put(split[Num], tmpCar);
+			return ;
+		}
+		// 출차
+		tmpCar.handleOut(split[Time]);
 	}
+
+
+	// 출차 안한 차들 업데이트
+	private void handle24() {
+		for (Car tmp : store.values()) {
+			if (tmp.status.equals("IN")) {
+				tmp.handleOut("23:59");
+			}
+		}
+	}
+
+
+
+	class Car implements Comparable<Car>{
+		String status;
+		String time;
+		String num;
+		int total;
+		int totalFee;
+
+		public Car(String[] split) {
+			this.num = split[Num];
+			this.time = split[Time];
+			this.status = "IN";
+			this.total = 0;
+			this.totalFee = 0;
+		}
+
+		public void handleIn(String time) {
+			this.time = time;
+			this.status = "IN";
+			// System.out.println("In : " + time + " " + this);
+		}
+
+		public void handleOut(String time) {
+			this.total += getNumTime(time) - getNumTime(this.time);
+			this.time = time;
+			this.status = "OUT";
+			// System.out.println("Out : " + time + " " + this);
+		}
+
+		private int getNumTime(String time) {
+			String[] split = time.split("\\:");
+			return Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
+		}
+
+		public String toString() {
+			return "num : " + num + " time : " + time + " total : " + total + " totalFee : " + totalFee;
+		}
+
+		public void calculate(int[] fees)  {
+			final int BasicT = 0;
+			final int BasicF = 1;
+			final int UnitT = 2;
+			final int UnitF = 3;
+
+			// 기본 요금
+			if (this.total <= fees[BasicT]) {
+				this.totalFee = fees[BasicF];
+				return;
+			}
+
+			// 초과 요금
+			this.totalFee = fees[BasicF] + getRound(this.total -
+					fees[BasicT], fees[UnitT]) * fees[UnitF];
+		}
+
+		private int getRound(int a, int b) {
+			return (int)Math.ceil(a*1.0 / b);
+		}
+
+		@Override
+		public int compareTo(Car o) {
+			return Integer.parseInt(this.num) - Integer.parseInt(o.num);
+		}
+	}
+
 }
