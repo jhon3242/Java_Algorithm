19c962bbb75384dd04dc89a75ae894b1a03aabff
jhon3242 <rldotnr2@gmail.com>
2023-06-19 11:55:52 +0900

    feat (programers) : 150368 (이모티콘 할인행사) 문제풀이 추가 2차 시도 (성공) 디버깅에 너무 많은 시간을 낭비했다. 함수화 해서 만들어야 디버깅에 좀 더 수월할 거 같다.

--- src/programmers/level2/p150368/Solution.java
+++ src/programmers/level2/p150368/Solution.java
@@ -2,55 +2,68 @@ package programmers.level2.p150368;
 
 import java.util.*;
 
+/**
+ System.out.println("x = " + x);
+ */
 class Solution {
-	private static int[][] users;
-	private static int[] result = new int[2];
+	private int[][] users;
+	private int[] emoticons;
+	private int N;
+	private int[] result = new int[]{-1, -1};
+
 
 	public int[] solution(int[][] users, int[] emoticons) {
+		N = emoticons.length;
 		this.users = users;
-		dfs(0, emoticons, new ArrayList<Integer>());
-		return this.result;
+		this.emoticons = emoticons;
+
+		// resultCheck();
+		dfs(0, new ArrayList<>());
+		return result;
 	}
 
-	private void updateMax(int[] emoticons, List<Integer> disArr) {
-		int[] result = new int[] {0, 0};
-		for (int i = 0; i < users.length; i++) {
-			int[] userInfo = users[i];
-			int tmpCose = 0;
-			for (int j = 0; j < emoticons.length; j++) {
-				if (disArr.get(j) >= userInfo[0]) {
-					tmpCose += emoticons[j] * (100 - disArr.get(j)) / 100;
-					// System.out.println(i + "번 유저 " + j + "번 이모티콘 구매 ");
-					if (tmpCose >= userInfo[1]) {
-						result[0]++;
-						tmpCose = 0;
-						break;
-					}
+	private void dfs(int level, List<Integer> sales) {
+
+		if (level == N) {
+			// System.out.println("sales = " + sales);
+			resultCheck(sales);
+			return;
+		}
+
+		for (int i = 10 ; i <= 40; i += 10) {
+			sales.add(i);
+			dfs(level + 1, sales);
+			sales.remove(sales.size() - 1);
+		}
+	}
+
+	private void resultCheck(List<Integer> sales) {
+		int[] result = new int[]{0, 0};
+
+		for (int[] user : users) {
+			int totalCost = 0;
+			for (int i = 0 ; i < N; i++){
+				int sale = sales.get(i);
+
+				// 구매하는 경우
+				if (user[0] <= sale) {
+					totalCost += emoticons[i] * (100.0 - sale) / 100;
+				}
+
+				// 멤버십 가입
+				if (totalCost >= user[1]) {
+					totalCost = 0;
+					result[0] += 1;
+					break;
 				}
 			}
-			result[1] += tmpCose;
+			result[1] += totalCost;
 		}
-		// System.out.println(disArr);
-		// System.out.println(Arrays.toString(result) + "\n");
-		if (result[0] > this.result[0] || (result[0] == this.result[0] && result[1] > this.result[1])) {
+
+
+		if (this.result[0] < result[0] ||
+				(this.result[0] == result[0] && this.result[1] < result[1])) {
 			this.result = result;
-			// System.out.println(Arrays.toString(this.result));
-		}
-	}
-
-	private void dfs(int level, int[] emoticons, List<Integer> disArr) {
-		int[] discount = new int[] {10, 20, 30, 40};
-
-		if (level == emoticons.length) {
-			// System.out.println(disArr);
-			updateMax(emoticons, disArr);
-			return ;
-		}
-
-		for (int dis : discount) {
-			disArr.add(dis);
-			dfs(level + 1, emoticons, disArr);
-			disArr.remove(disArr.size() - 1);
 		}
 	}
 }
